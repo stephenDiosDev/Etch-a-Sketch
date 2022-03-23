23466aa197ddbb8f8fb7b8fbd8edebc32ffccc50
Stephen Dios <stephendiosdeveloper@gmail.com>
2022-03-23 17:25:31 -0400

    Added square grid
    Responsiveness not great, resizing page makes the grid rectangle

--- mainScript.js
+++ mainScript.js
@@ -1,1 +1,20 @@
 console.log("Hello, World!");
+
+let sketchpad = document.querySelector(".sketchpad");
+
+
+for(let row = 0; row < 16; row++) {
+    //add a div to hold the row
+    let rowDiv = document.createElement('div');
+    rowDiv.classList.add("row-div");
+
+    //fill row div with 16 divs
+    for(let j = 0; j < 16; j++) {
+        let pixel = document.createElement('div');
+        pixel.classList.add("pixel");
+        rowDiv.appendChild(pixel);
+    }
+
+    //now that rowDiv is full, add to document
+    sketchpad.appendChild(rowDiv);
+}
