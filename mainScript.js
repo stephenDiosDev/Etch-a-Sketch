3561d51baa34cf4c7c511e3490c308715415f303
Stephen Dios <stephendiosdeveloper@gmail.com>
2022-03-29 21:30:32 -0400

    Clear grid lets you choose custom number of cells

--- mainScript.js
+++ mainScript.js
@@ -2,35 +2,7 @@ console.log("Hello, World!");
 
 let sketchpad = document.querySelector(".sketchpad");
 
-
-for(let row = 0; row < 16; row++) {
-    //add a div to hold the row
-    let rowDiv = document.createElement('div');
-    rowDiv.classList.add("row-div");
-
-    //fill row div with 16 divs
-    for(let j = 0; j < 16; j++) {
-        let pixel = document.createElement('div');
-        pixel.classList.add("pixel");
-
-        pixel.addEventListener('mouseenter', function(e) {
-            //console.log("MOUSE ENTER: ");
-            //console.log(e);
-            pixel.style.backgroundColor = "pink";
-        });
-
-        pixel.addEventListener('mouseleave', function(e) {
-            //console.log("MOUSE LEAVE: ");
-            //console.log(e);
-            pixel.style.backgroundColor = "white";
-        });
-    
-        rowDiv.appendChild(pixel);
-    }
-
-    //now that rowDiv is full, add to document
-    sketchpad.appendChild(rowDiv);
-}
+addGrid(sketchpad, 16);
 
 let clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener("mouseenter", function(e) {
@@ -51,38 +23,71 @@ clearBtn.addEventListener("mouseup", function(e) {
 });
 
 function clearGrid() {
-    //loop thru all row-div from sketchpad
-    //loop thru all pixel from current row-div
-    //set pixel background colour to white
-    let sketchpad = document.querySelectorAll(".sketchpad");
-    
-    //console.log(sketchpad[0]);
-    let rows = sketchpad[0].getElementsByClassName("row-div");
-    //console.log(rows);
+    let sketchpad = document.querySelector(".sketchpad");
+    let rows = sketchpad.getElementsByClassName("row-div");
 
     for(let i = 0; i < rows.length; i++) {
         let row = rows[i];
 
         row.childNodes.forEach(pixel => {
-            //console.log(pixel);
             pixel.style.backgroundColor = "white";
         });
+    }
 
+    newGrid(sketchpad, prompt("Enter a new grid width between 0 and 100", 10));
+}
 
+function newGrid(sketchpad, size) {
+    let actualWidth = 1;
+    if(size > 1 && size < 101) {
+        actualWidth = size;
+    }
+    else if(size > 101) {
+        actualWidth = 100;
+    }
+
+    while(sketchpad.firstChild) {
+        sketchpad.removeChild(sketchpad.firstChild);
     }
     
+    //add new grid
+    addGrid(sketchpad, actualWidth);
+}
 
-
-
-    console.log("GRID CLEARED");
+function addGrid(gridElement, size) {
+    for(let row = 0; row < size; row++) {
+        //add a div to hold the row
+        let rowDiv = document.createElement('div');
+        rowDiv.classList.add("row-div");
+    
+        //fill row div with divs
+        for(let j = 0; j < size; j++) {
+            let pixel = document.createElement('div');
+            pixel.classList.add("pixel");
+    
+            pixel.addEventListener('mouseenter', function(e) {
+                pixel.style.backgroundColor = "pink";
+            });
+    
+            pixel.addEventListener('mouseleave', function(e) {
+                pixel.style.backgroundColor = "white";
+            });
+        
+            rowDiv.appendChild(pixel);
+        }
+    
+        //now that rowDiv is full, add to document
+        gridElement.appendChild(rowDiv);
+    }
 }
 
 /*
     TODO
-        [] Button to clear colour on grid
-            - button is added, add functionality and responsiveness
+        [X] Button to clear colour on grid
         [] Button to make a new grid of size 1 to 100 (1x1 to 100x100)
         [] RGB colour selector that changes drawable colour
         [] Make a grid that does not resize
             - Ideally it can grow evenly up, but cannot shrink past a certain size 
+            - The details page does mention a 960px width and 960px height, so we 
+                can just make a hardcoded height and width
 */
